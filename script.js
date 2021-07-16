47b9d26cd163da2841fab1b7d395141a300aef95
Chris Rutledge <hi@rutledgeismyname.com>
2021-07-16 08:45:56 -0400

    update flight num - passenger count - price of flight

--- script.js
+++ script.js
@@ -2,14 +2,26 @@
 // Creating a Airplane Booking App 
 const bookings = [];
 
-const createBooking = function(flightNum, numPassengers, price){
+const createBooking = function(
+    flightNum, 
+    numPassengers = 7,
+     price = 199 * numPassengers
+     ){
+
+    // ES5 way to set default prams, ES6 Way is above ^^^
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+
     const booking = {
         flightNum,
         numPassengers,
         price
-    }
+    };
     console.log(booking);
     bookings.push(booking);
-}
+};
 
-createBooking('LH123', 85, '$450');
+createBooking('LH123');
+createBooking('LH123', 6);
+createBooking('LH123', 55, undefined)
+// Var | Flight Num | Passenger Count | Price of Flight
